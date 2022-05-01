77a3af654790305ba8783ebde51aefc59dd8f340
John Siryj <JohnS@provoke.co.nz>
2022-05-02 09:52:18 +1200

    Add the stuff needed to deploy (hopefully...)

---
+++ pages/factory.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { models, Report, Embed, service, Page } from "powerbi-client";
+import { PowerBIEmbed } from "powerbi-client-react";
+import styles from '../styles/App.module.css';
+
+function App() {
+  const [responseConfig, setResponseConfig] = useState({});
+  useEffect(() => {
+    fetch("https://getembedinfo.azurewebsites.net/api/PostOurEmbedInfo",
+    {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+          'Access-Control-Allow-Headers': '*'
+        },
+        body: JSON.stringify(
+          {
+            "report": "6597db52-4566-4838-ab1d-13c933a957a5"
+          }
+        ),
+    }).then((res) => res.json())
+      .then((response) => {
+        setResponseConfig(response);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Power BI Embed:</h1>
+        <div> </div>
+      <PowerBIEmbed
+          embedConfig={{
+            type: "report", // Supported types: report, dashboard, tile, visual and qna
+            id: responseConfig.ReportId,
+            embedUrl: responseConfig.EmbedUrl,
+            accessToken: responseConfig.EmbedToken,
+            tokenType: models.TokenType.Embed,
+            settings: {
+              panes: {
+                filters: {
+                  expanded: false,
+                  visible: false,
+                },
+                pageNavigation: {
+                  visible: false,
+                },
+              },
+              background: models.BackgroundType.Transparent,
+            },
+          }}
+          eventHandlers={
+            new Map([
+              [
+                "loaded",
+                function () {
+                  console.log("Report loaded");
+                },
+              ],
+              [
+                "rendered",
+                function () {
+                  console.log("Report rendered");
+                },
+              ],
+              [
+                "error",
+               function (event) {
+                  console.log(event.detail);
+                },
+              ],
+            ])
+          }
+          cssClassName={styles.reportstyleclass}
+
+          getEmbeddedComponent={(embeddedReport) => {
+            window.report = embeddedReport; 
+          }}
+        />
+      </header>
+    </div>
+    
+  );
+}
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import SmallCard from '../components/SmallCard';
 import { projectIcons } from '../components/Icons';
+import Link from 'next/link';
 
 import { projects } from '../utils/projectsData';
 
 const Home = () => (
   <div className="home">
     <h1>What Can I Deploy to Static Apps?</h1>
+    <Link href="/factory"><a>Factory Example</a></Link>
     <div className="card-grid">
       {projects.map((project) => {
         const Icon = projectIcons[project.id];
